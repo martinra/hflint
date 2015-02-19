eef71929c3e9ad93ee11d2424f0594dbb4d0b94c
Martin Raum <martin@raum-brothers.eu>
2015-02-19 12:58:35 +0100

    Updata tests for FMPQPoly

--- test/FMPQPolyTests.hs
+++ test/FMPQPolyTests.hs
@@ -5,10 +5,15 @@
 module FMPQPolyTests
 where
 
-import Control.Arrow ( second )
+import Control.Arrow ( (***) )
+import Data.Composition ( (.:) )
+import Data.Function ( on )
 import Data.List ( delete
-                 , intercalate )
+                 , intercalate
+                 , dropWhileEnd
+                 )
 import Data.List.Split ( splitOn )
+import qualified Data.Vector as V
 
 import Test.Tasty ( testGroup
                   , TestTree
@@ -17,6 +22,7 @@ import Test.Tasty ( testGroup
 import qualified Test.Tasty.SmallCheck as SC
 import qualified Test.Tasty.QuickCheck as QC
 import qualified Test.Tasty.HUnit as HU
+import Test.Tasty.HUnit ( (@?=) )
 
 import HFlint.FMPQ
 import HFlint.FMPQPoly
@@ -25,7 +31,7 @@ import qualified TestHFlint.Utils as U
 
 
 fmpqPolyTestGroup :: TestTree
-fmpqPolyTestGroup = testGroup "FMPQ Tests" []
+fmpqPolyTestGroup = testGroup "FMPQPoly Tests" [ properties, unitTests ]
 
 -- We need to specify the type, so that a is not specialized when infering the
 -- type on first occurence of equal and equal2
@@ -45,5 +51,20 @@ testProperty s p = testGroup ("(QuickCheck & SmallCheck)")
 properties :: TestTree
 properties = testGroup "Properties"
   [ -- Eq instance
-    testProperty "Eq" $ equal2 (==) (==)
+    SC.testProperty "Eq" $ equal2
+    ((==) `on` V.toList . V.reverse . V.dropWhile (0==) . V.reverse . V.fromList) (==)
   ] 
+
+
+unitTests :: TestTree
+unitTests = testGroup "Unit Tests"
+  [ -- Show instance
+    HU.testCase "Show Deg -1" $ 
+      show (fromRationals []) @?= "0"
+  , HU.testCase "Show Deg 0" $ 
+      show (fromRationals [1/2]) @?= "1/2"
+  , HU.testCase "Show Deg 1" $ 
+      show (fromRationals [1/2,1/4]) @?= "1/4*T+1/2"
+  , HU.testCase "Show Deg 2" $ 
+      show (fromRationals [2/7,1/4,3/5]) @?= "3/5*T^2 + 1/4*T + 2/7"
+  ]
