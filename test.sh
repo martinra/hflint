05319329781ed5cd3b15fb32df43ec58d6218a69
Martin Raum <martin@raum-brothers.eu>
2015-02-19 12:12:49 +0100

    Delete test.sh

--- test.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-cabal configure --extra-lib-dirs=$flint/lib --extra-lib-dirs=$gf2x/lib --extra-lib-dirs=$mpfr/lib --extra-lib-dirs=$ntl/lib --extra-lib-dirs=$gmp/lib --disable-library-profiling
-cabal test
