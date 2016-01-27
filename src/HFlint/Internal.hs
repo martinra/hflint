c13323916560ebb2cf077f0fd6dd160c70419008
Martin Westerholt-Raum <martin@raum-brothers.eu>
2016-01-27 11:38:21 +0100

    Expose some internals

---
+++ src/HFlint/Internal.hs
@@ -0,0 +1,23 @@
+module HFlint.Internal
+  ( module Context
+
+  , module Flint
+  , module FlintPrim
+  , module FlintWithContext
+
+  , module Lift
+  , module LiftCtx
+  , module LiftPrim
+  )
+where
+
+
+import HFlint.Internal.Context as Context
+
+import HFlint.Internal.Flint as Flint
+import HFlint.Internal.FlintPrim as FlintPrim
+import HFlint.Internal.FlintWithContext as FlintWithContext
+
+import HFlint.Internal.Lift as Lift
+import HFlint.Internal.LiftCtx as LiftCtx
+import HFlint.Internal.LiftPrim as LiftPrim
