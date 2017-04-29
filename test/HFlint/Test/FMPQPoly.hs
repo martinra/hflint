fa961d541bcf7837d4ebaca91d743fc3f6d162d5
Martin Raum <martin@raum-brothers.eu>
2017-04-29 09:33:59 +0200

    update FMP_Poly intertwinings tests

--- test/HFlint/Test/FMPQPoly.hs
+++ test/HFlint/Test/FMPQPoly.hs
@@ -7,20 +7,11 @@ where
 
 import Data.Function ( on )
 import Math.Structure.Tasty
-import qualified Data.Vector as V
-
-import Test.Tasty ( testGroup
-                  , TestTree
-                  )
-
-import qualified Test.Tasty.HUnit as HU
-import Test.Tasty.HUnit ( (@?=) )
+import Test.Tasty ( testGroup , TestTree )
+import Test.Tasty.HUnit ( testCase, (@?=) )
 
 import HFlint.FMPQPoly
 import HFlint.FMPZPoly
-
-import HFlint.Test.Utility.FMPQPoly
-import qualified HFlint.Test.Utility.Intertwine as I
 
 
 tests :: TestTree
@@ -32,16 +23,14 @@ tests = testGroup "FMPQPoly tests"
 properties :: TestTree
 properties = testGroup "Properties"
   [ -- Eq instance
-    testPropertyQSnC 2 "Eq" $ equal2
-    ( let
-      dropTrailingZeros = V.toList . V.reverse . V.dropWhile (0==) .
-                          V.reverse . V.fromList
-      in (==) `on` dropTrailingZeros )
-    (==)
+    testPropertyQSnC 2 "Eq" $
+      intertwiningInnerPairing fromRationals
+      ((==) `on` reverse . dropWhile (0==) . reverse)
+      (==)
 
     -- conversion from and to FMPZPoly
-  , testPropertyQSC "fromFMPZPoly" $ I.equal
-      (id :: [Integer] -> [Integer]) undefined
+  , testPropertyQSC "fromFMPZPoly" $
+      intertwiningMorphisms (id :: [Integer] -> [Integer])
       (fromRationals . map fromInteger)
       (fromFMPZPoly . fromIntegers)
 
@@ -61,10 +50,12 @@ properties = testGroup "Properties"
 unitTests :: TestTree
 unitTests = testGroup "Unit Tests"
   [ -- Show instance
-    HU.testCase "Show Deg -1" $ 
+    testCase "Show Deg -1" $ 
       show (fromRationals []) @?= "0"
-  , HU.testCase "Show Deg 0" $ 
+  , testCase "Show Deg 0" $ 
       show (fromRationals [1/2]) @?= "1/2"
-  , HU.testCase "Show Deg 1" $ show (fromRationals [1/2,1/4]) @?= "1/4*T+1/2" , HU.testCase "Show Deg 2" $ 
+  , testCase "Show Deg 1" $
+      show (fromRationals [1/2,1/4]) @?= "1/4*T+1/2"
+  , testCase "Show Deg 2" $ 
       show (fromRationals [2/7,1/4,3/5]) @?= "3/5*T^2 + 1/4*T + 2/7"
   ]
