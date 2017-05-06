ad3c6286c2e451f35a1d99b368c348b2e499a072
Martin Raum <martin@raum-brothers.eu>
2017-05-06 08:36:19 +0200

    add Prelude

---
+++ src/HFlint/Utility/Prelude.hs
@@ -0,0 +1,33 @@
+module HFlint.Utility.Prelude
+  ( module Import
+  )
+where
+
+import Prelude as Import hiding
+  ( (+), (-), negate, subtract
+  , (*), (/), recip, (^), (^^)
+  , gcd, lcm
+  , quotRem, quot, rem
+  )
+
+import Control.Arrow       as Import
+  ( (&&&), (***), first, second )
+import Control.Applicative as Import
+import Control.DeepSeq     as Import
+import Control.Monad       as Import
+import Control.Monad.Zip   as Import
+import Data.Composition    as Import
+  ( (.:), (.:.) )
+import Data.Either         as Import
+import Data.Foldable       as Import
+import Data.Functor        as Import
+import Data.Maybe          as Import
+import Data.Monoid         as Import
+import Data.Proxy          as Import
+import Data.Traversable    as Import
+import Data.Vector as Import
+  ( Vector )
+import GHC.Exts            as Import
+  ( IsList )
+import Math.Structure      as Import
+import Numeric.Natural     as Import
